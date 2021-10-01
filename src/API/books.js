0e9ff47980d83bada2d57cc9d742d42778fa8e9d
Marius Kuzminskas <marius.kuzminskas@ca.com>
2021-10-01 12:06:56 +0300

    joi validation

--- src/API/books.js
+++ src/API/books.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mysql = require('mysql2/promise');
+const joi = require('joi');
 const dbConfig = require('../dbConfig');
 
 const router = express.Router();
@@ -16,10 +17,46 @@ router.get('/', async (req, res) => {
     res.status(500).send({ error: 'Error getting books' });
   }
 });
-router.post('/add', (req, res) => {
+router.post('/add', async (req, res) => {
   // sukurti knyga
   console.log('we got data to create book', req.body);
-  res.send({ msg: 'Trying to add a book' });
+  // validate with joy
+  const newBookSchema = joi.object({
+    title: joi.string().min(3).max(50).required(),
+    author: joi.string().min(3).max(20).required(),
+    year: joi.number().greater(1000).less(2021).required(),
+    image: joi.string().min(3).max(100).required(),
+  });
+
+  let formValid = false;
+  try {
+    const validationResult = await newBookSchema.validateAsync(req.body, {
+      abortEarly: false,
+    });
+    // res.send({ msg: 'inputs valid', validationResult });
+    formValid = true;
+  } catch (err) {
+    formValid = false;
+    console.log('err', err);
+    res.status(400).send({
+      error: 'please check inputs',
+      err: err.details,
+    });
+  }
+
+  if (!formValid) return;
+  console.log('does it show after errror');
+
+  try {
+    const conn = await mysql.createConnection(dbConfig);
+    const sql = 'INSERT INTO books (title,author,year,image) VALUES(?,?,?,?)';
+    const [result] = await conn.execute(sql, Object.values(req.body));
+    res.send({ msg: 'book added' });
+    await conn.end();
+  } catch (error) {
+    res.status(500).send({ error: 'Something went wrong' });
+  }
+  // res.send({ msg: 'Trying to add a book' });
 });
 router.delete('/:id', (req, res) => {
   // istrinti knyga knyga
